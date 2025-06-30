96ab1996e1351676681968bca37515443b386d88
VISHNU KUMAR DHONI <vishnu_24a12res771@iitp.ac.in>
2025-06-30 08:09:33 +0530

    Update lms_db.sql

--- lms_db.sql
+++ lms_db.sql
@@ -1,28 +1,5 @@
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19  Distrib 10.11.11-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: lms_db
--- ------------------------------------------------------
--- Server version	10.11.11-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `books`
---
-
 DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `books` (
   `book_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -36,14 +13,9 @@ CREATE TABLE `books` (
   PRIMARY KEY (`book_id`),
   UNIQUE KEY `isbn` (`isbn`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `books`
---
 
 LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+
 INSERT INTO `books` VALUES
 (1,'1984','George Orwell','Secker & Warburg','9780451524935',1949,5,5,'2025-06-10 19:39:35'),
 (2,'To Kill a Mockingbird','Harper Lee','J.B. Lippincott & Co.','9780061120084',1960,4,4,'2025-06-10 19:39:35'),
@@ -51,16 +23,11 @@ INSERT INTO `books` VALUES
 (4,'The Catcher in the Rye','J.D. Salinger','Little, Brown and Company','9780316769488',1951,3,3,'2025-06-10 19:39:35'),
 (5,'The Hobbit','J.R.R. Tolkien','George Allen & Unwin','9780547928227',1937,5,5,'2025-06-10 19:39:35'),
 (6,'Fahrenheit 451','Ray Bradbury','Ballantine Books','9781451673319',1953,4,4,'2025-06-10 19:39:35');
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `issued_books`
---
-
 DROP TABLE IF EXISTS `issued_books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `issued_books` (
   `issue_id` int(11) NOT NULL AUTO_INCREMENT,
   `book_id` int(11) NOT NULL,
@@ -74,26 +41,16 @@ CREATE TABLE `issued_books` (
   CONSTRAINT `issued_books_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `issued_books_ibfk_2` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `issued_books`
---
 
 LOCK TABLES `issued_books` WRITE;
-/*!40000 ALTER TABLE `issued_books` DISABLE KEYS */;
+
 INSERT INTO `issued_books` VALUES
 (1,6,1,'2025-06-10','2025-06-24','2025-06-12');
-/*!40000 ALTER TABLE `issued_books` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `members`
---
-
 DROP TABLE IF EXISTS `members`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `members` (
   `member_id` int(11) NOT NULL AUTO_INCREMENT,
   `full_name` varchar(255) NOT NULL,
@@ -104,26 +61,16 @@ CREATE TABLE `members` (
   PRIMARY KEY (`member_id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `members`
---
 
 LOCK TABLES `members` WRITE;
-/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+
 INSERT INTO `members` VALUES
-(1,'vishnu kumar','kumarvishun24@gmai.com','8102988587','2025-06-10','active');
-/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+(1,'vishnu kumar','kumarvis****@gmai.com','81029*****','2025-06-10','active');
+
 UNLOCK TABLES;
 
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `member_id` int(11) DEFAULT NULL,
@@ -136,26 +83,10 @@ CREATE TABLE `users` (
   KEY `member_id` (`member_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
 INSERT INTO `users` VALUES
 (1,1,'vishnu-iitp','4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2','member','2025-06-10 19:40:13');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-06-13 19:26:35
